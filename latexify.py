0a7f2a70f8990be2c8e5d2f9c7a7b48f405427ba
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-03-17 00:48:44 -0500

    use string as value for `text.latex.preamble` param

--- latexify.py
+++ latexify.py
@@ -40,7 +40,7 @@ def latexify(fig_width=None, fig_height=None, font_size=12, columns=2):
     # Note that this does not hold if you put two figures next to each other using
     # minipage. You need to use subplots.
     params = {'backend': 'ps',
-              'text.latex.preamble': ['\\usepackage{gensymb}'],
+              'text.latex.preamble': '\\usepackage{gensymb}',
               'axes.labelsize': font_size, # fontsize for x and y labels (was 12 and before 10)
               'axes.titlesize': font_size,
               'font.size': font_size, # was 12 and before 10
